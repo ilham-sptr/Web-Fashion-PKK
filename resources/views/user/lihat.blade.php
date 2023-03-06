9c353accfa7b0f54fff1000a26fe7cf118fb7efa
badzlan <badzlandhabith05@gmail.com>
2023-03-06 11:27:55 +0700

    update whatsapp href produk

--- resources/views/user/lihat.blade.php
+++ resources/views/user/lihat.blade.php
@@ -11,7 +11,7 @@
           <h3 class="fw-bold">Selamat!</h3>
           <p>Anda telah berhasil mengisi data pengiriman.</p>
           <p>Untuk melakukan pembayaran disini.</p>
-          <a href="https://wa.me/62{{substr($clothing->nomor_telepon, 1, 12)}}?text=Permisi%20Admin%20Kepada%20:%0D%0AEmail%20:%0D%0A" class="btn btn-success" target="_blank"><i class='bx bxl-whatsapp'></i> Hubungi Kami via WhatsApp</a>
+          <a href="https://wa.me/62{{substr($clothing->nomor_telepon, 1, 12)}}?text=Halo, Apakah produk {{ $clothing->title }} masih tersedia?" target="_blank"><i class='bx bxl-whatsapp'></i> Hubungi Kami via WhatsApp</a>
           {{-- <a href="#" class="btn btn-primary">Kembali ke Beranda</a> --}}
         </div>
       </div>
